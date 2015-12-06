c6149dcbe64fd096a4ef52ef29969810622bab99
Thomas Khyn <thomas@ksytek.com>
2015-12-07 12:02:20 +1300

    Enabling tests for decorators handling without dependency injection

--- test/dependency-injection.spec.js
+++ test/dependency-injection.spec.js
@@ -41,11 +41,8 @@ describe('dependency injection', () => {
             }
         }
 
-        beforeAll(()=>{
-            instanceProto = {};
-        });
-
         beforeEach(() => {
+            instanceProto = {};
             instance = Object.create(instanceProto);
             container = new Container();
 
@@ -54,11 +51,12 @@ describe('dependency injection', () => {
             handler.invoke = handler.invokeImpl;
         });
 
+        it('intercepts handler.invoke', () => {
+            handlerCreationCb(handler);
+            expect(handler.invoke).not.toBe(handler.invokeImpl);
+        });
+
         describe('without dispatcher injected', () => {
-            it('does not intercepts handler.invoke', () => {
-                handlerCreationCb(handler);
-                expect(handler.invoke).toBe(handler.invokeImpl);
-            });
 
             it('runs original invoke method', () => {
                 handlerCreationCb(handler);
@@ -72,53 +70,41 @@ describe('dependency injection', () => {
                 expect(result).toBe(instance);
             });
 
+            describe('for instance prototype with metadata', () => {
 
-            /**
-             * Future feature: does not require Dispatcher to be injected
-             * to be able to use handle or waitFor decorators
-             */
-            //
-            //describe('for instance prototype with metadata', () => {
-            //
-            //    beforeEach(() => {
-            //        instanceProto[Symbols.metadata] = new Metadata();
-            //    });
-            //
-            //    it('sets instance[Symbols.instanceDispatcher]', () => {
-            //        handlerCreationCb(handler);
-            //        expect(instance[Symbols.instanceDispatcher]).toBeUndefined();
-            //        handler.invoke(container);
-            //        expect(instance[Symbols.instanceDispatcher]).toBeDefined();
-            //    });
-            //
-            //    it('runs instance Disptacher.registerMetadata', () => {
-            //        spyOn(Dispatcher.prototype, 'registerMetadata');
-            //        handlerCreationCb(handler);
-            //        handler.invoke(container, []);
-            //        expect(Dispatcher.prototype.registerMetadata).toHaveBeenCalled();
-            //    });
-            //
-            //    it('intercepts instance deactivators', () => {
-            //        spyOn(LifecycleManager, 'interceptInstanceDeactivators');
-            //        handlerCreationCb(handler);
-            //        handler.invoke(container, []);
-            //        expect(LifecycleManager.interceptInstanceDeactivators).toHaveBeenCalledWith(instance);
-            //    });
-            //});
+                beforeEach(() => {
+                    instanceProto[Symbols.metadata] = new Metadata();
+                });
+
+                it('sets instance[Symbols.instanceDispatcher]', () => {
+                    handlerCreationCb(handler);
+                    expect(instance[Symbols.instanceDispatcher]).toBeUndefined();
+                    handler.invoke(container);
+                    expect(instance[Symbols.instanceDispatcher]).toBeDefined();
+                });
+
+                it('runs instance Disptacher.registerMetadata', () => {
+                    spyOn(Dispatcher.prototype, 'registerMetadata');
+                    handlerCreationCb(handler);
+                    handler.invoke(container, []);
+                    expect(Dispatcher.prototype.registerMetadata).toHaveBeenCalled();
+                });
+
+                it('intercepts instance deactivators', () => {
+                    spyOn(LifecycleManager, 'interceptInstanceDeactivators');
+                    handlerCreationCb(handler);
+                    handler.invoke(container, []);
+                    expect(LifecycleManager.interceptInstanceDeactivators).toHaveBeenCalledWith(instance);
+                });
+            });
 
         });
 
-        // with dispatcher injected
         describe('with dispatcher injected', () => {
 
 
             beforeEach(() => {
                 handler.dependencies.push(Dispatcher);
-            });
-
-            it('intercepts handler.invoke', () => {
-                handlerCreationCb(handler);
-                expect(handler.invoke).not.toBe(handler.invokeImpl);
             });
 
             it('runs original invoke method', () => {
@@ -181,7 +167,6 @@ describe('dependency injection', () => {
                 });
             });
         });
-        ///////////////////////////////
 
     });
 
